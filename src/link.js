0f894384de1749941b6b5af9e8438bf1e5ab3f96
aznoqmous <paul.landreau931v@gmail.com>
2021-02-19 10:47:00 +0100

    better link handle

--- src/link.js
+++ src/link.js
@@ -1,14 +1,34 @@
 export default class Link {
+
   constructor(url, host){
+    this.init(url, host)
+
     this.pagesIn = []
+    this.status = null
+  }
+
+  init(url, host){
+
+    url = url.replace(/#.*?$/, '') // rm anchor
+    url = url.replace(/\?.*?$/, '') // rm params
+
+    if(url.match(/^[^\/]*?\./)) {
+      let filesExt = '.html|.php'
+      if(!url.match(`/[${filesExt}]/`)) {
+        if(!url.match(/^http/)) url = `http://${url}`
+      }
+    }
+
     url = decodeURIComponent(JSON.parse('"' + url + '"'))
-    if(!url.match('http')) url = (url.length && url != '/') ? `http://${host}/${url}` : `http://${host}`
+    if(!url.match(/^http/)) url = (url.length && url != '/') ? `http://${host}/${url}` : `http://${host}`
+
     this.url = url
     this.host = (new URL(this.url)).host
     this.isExternal = (this.host != host)
-    this.status = null
   }
+
   addPageRef(page){
     if(!this.pagesIn.includes(page)) this.pagesIn.push(page)
   }
+
 }
